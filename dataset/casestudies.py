c159fba377d2c256f7814b223102236109daf1a2
goyalkushal87 <goyalkushal87@gmail.com>
2019-12-02 04:51:59 +0530

    1.1
    bbb

--- dataset/casestudies.py
+++ dataset/casestudies.py
@@ -13,18 +13,31 @@ import utility as ut
 
  
 def main():
-  columnName = "permalink"  
-  df =ut.utility.readFile("companies.txt", "\t","ISO-8859-1")
-  duplicate = ut.utility.checkDuplicate(df, columnName)
-  if(duplicate is not None):
-      print(duplicate)
-      # check specific column
-      count =ut.utility.checkCount(duplicate,columnName)
-      print(count)
-  else :
-      print(df)
-     
-  # remove unwanted chaarcter
-
+  # dataframe companies  
+  dataframe_companies = ut.utility.getDataframe("companies.txt","permalink", "\t","ISO-8859-1")  
+  companies_count = ut.utility.checkCount(ut.utility.checkDuplicate(dataframe_companies,"permalink"),"permalink")
+  print(companies_count)
+  print("***********unique********************")
+  print(dataframe_companies.permalink.nunique())
+  print("***********nunique********************")
+  # dataframe Funding round details: 
+  dataframe_funding = ut.utility.getDataframe("rounds2.csv","company_permalink", ",","ISO-8859-1")
+  funding_count = ut.utility.checkCount(ut.utility.checkDuplicate(dataframe_funding,"funding_round_permalink"),"funding_round_permalink")
+  print(funding_count)
+  print("***********unique********************")
+  print(dataframe_funding.funding_round_permalink.nunique())
+  print("***********nunique********************")
+  
+  
+  #get unique count difference between dataframe
+  #Are there any companies in the rounds2 file which are not  present in companies ?
+  diff_companies = ut.utility.getuniquecountDiff(dataframe_companies,dataframe_funding,"permalink","company_permalink")
+  print("Are there any companies in the rounds2 file which are not  present in companies ? ", diff_companies)
+  
+  # dataframe merging 
+  master_frame = pd.merge(dataframe_companies, dataframe_funding, how ='inner', left_on ='permalink',right_on="company_permalink") 
+  print(master_frame.count())
+  
+  
 if  __name__ =='__main__':
     main()    
--- dataset/utility.py
+++ dataset/utility.py
@@ -7,9 +7,18 @@ Created on Sun Dec  1 09:56:46 2019
 
 import numpy as np
 import pandas as pd
+import chardet
+import re
 
 class utility:
-    
+    # function to test if a file is in unicode
+    def isItUnicode(filename):
+        with open(filename, 'rb') as f:
+            encodingInfo = chardet.detect(f.read())
+            if "UTF" not in encodingInfo['encoding']: 
+                    print("This isn't Unicode! It's", encodingInfo['encoding'])
+            else: 
+                print("Yep, it's Unicode.")
     
     def readFile(location, separator, encoding):
         df = pd.read_csv(location, sep=separator,encoding = encoding)
@@ -23,5 +32,44 @@ class utility:
         
         
     def checkCount(dataframe, columnName):
-        print(dataframe)
-        return dataframe[columnName].count()
+         if(dataframe is not None):
+                count = dataframe[columnName].count()
+                return count
+    
+ #   def removeUnwantedCharacer(dataframe)
+    def strip_character(dataCol):
+         r = re.compile(r'[^a-zA-Z !@#$%&*_+-=|\:";<>,./()[\]{}\']')
+         return r.sub('', dataCol)
+     
+    def getDataframe(filelocation,UniqueID,separator,encoding):
+            
+            columnName = UniqueID #"permalink"  "companies.txt" "\t" "ISO-8859-1"
+            df = utility.readFile(filelocation,separator,encoding)
+            df[columnName] = df[columnName].astype(str).str.lower()
+            df[columnName] = df[columnName].apply(utility.strip_character)
+            return df
+            
+    def getuniquecountDiff(dataframe1,dataframe2,column1,column2):
+        df1 = dataframe1[column1].unique() 
+        df2 = dataframe2[column2].unique()
+        count =len(set(df1) ^ set(df2))
+        print("**************set 2********************************")
+      
+        print("Symmetric difference :", count )
+        return count
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
